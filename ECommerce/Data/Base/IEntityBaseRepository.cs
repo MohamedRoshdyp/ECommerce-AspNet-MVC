1af788cd0858c845b59d7cdbafb249ee9fe85dc1
MohamedRoshdyp <rehame.roshdy.alyamany@gmail.com>
2022-05-01 10:13:56 +0200

    End Part Products

--- ECommerce/Data/Base/IEntityBaseRepository.cs
+++ ECommerce/Data/Base/IEntityBaseRepository.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace ECommerce.Data.Base
@@ -6,7 +8,9 @@ namespace ECommerce.Data.Base
     public interface IEntityBaseRepository<T> where T : class,IBaseEntity
     {
         Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] include);
         Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] include);
 
         Task CreateAsync(T entity);
         Task UpdateAsync(T entity);
